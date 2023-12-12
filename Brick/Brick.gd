f5b98ba8ab5f28761fab6a7a100c49c1953cba3d
MailoudaW <142926556+MailoudaW@users.noreply.github.com>
2023-12-11 20:00:54 -0500

    Complete

--- Brick/Brick.gd
+++ Brick/Brick.gd
@@ -3,12 +3,26 @@ extends StaticBody2D
 var score = 0
 var new_position = Vector2.ZERO
 var dying = false
+var time_appear = 0.5
+var time_fall = 0.8
+var time_rotate = 1.0
+
+var time_a = 0.8
+var time_s = 1.2
+var time_v = 1.5
+
+var tween
+ 
 
 var powerup_prob = 0.1
 
 func _ready():
 	randomize()
-	position = new_position
+	position.x = new_position.x
+	position.y = -100
+	tween = create_tween()
+	tween.tween_property(self, "position", new_position, 0.5 + randf()*2).set_trans(Tween.TRANS_BOUNCE)
+
 	if score >= 100:
 		$ColorRect.color = Color8(224,49,49,255)
 	elif score >= 90:
@@ -26,7 +40,7 @@ func _ready():
 
 
 func _physics_process(_delta):
-	if dying and not $Confetti.emitting:
+	if dying and not $Confetti.emitting and not tween:
 		queue_free()
 
 func hit(_ball):
@@ -43,8 +57,12 @@ func die():
 	get_parent().check_level()
 	if randf() < powerup_prob:
 		var Powerup_Container = get_node_or_null("/root/Game/Powerup_Container")
-		if Powerup_Container != null:
-			var Powerup = load("res://Powerups/Powerup.tscn")
-			var powerup = Powerup.instantiate()
-			powerup.position = position
-			Powerup_Container.call_deferred("add_child", powerup)
+
+	if tween:
+		tween.kill()
+		tween = create_tween().set_parallel(true)
+		tween.tween_property(self, "position", Vector2(position.x, 1000), time_fall).set_trans(Tween.TRANS_EXPO).set_ease(Tween.EASE_IN)
+		tween.tween_property(self, "rotation", -PI + randf()*2*PI, time_rotate).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN_OUT)
+		tween.tween_property($ColorRect, "color:a", 0, time_a)
+		tween.tween_property($ColorRect, "color:s", 0, time_s)
+		tween.tween_property($ColorRect, "color:v", 0, time_v)
